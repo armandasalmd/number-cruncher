7f6967b295a3d43203aa90ac306a9777d795d1fb
barkausa <barkausa@coventry.ac.uk>
2019-09-29 01:14:57 +0100

    initial commit

---
+++ dropfunctions.js
@@ -0,0 +1,14 @@
+var dropzone;
+
+function setup() {
+	createCanvas(200, 200);
+	background(0);
+	dropzone = select('#dropzone');
+	dropzone.dragOver(h);
+}
+
+function h() {
+	dropzone.style('background-color:');
+}
+
+console.log('I am here');
